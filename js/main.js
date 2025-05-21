36c99860dc2578ddf26087d43c362d08cc1b6845
Hugo Lopez <destroyermaster10@gmail.com>
2025-05-20 21:46:12 -0600

    Fix: Index HTML & Main JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     initCharts();
   });
-  
+ document.getElementById('exportDashboardBtn').addEventListener('click', () => {
+      if (typeof exportDashboardData === 'function') {
+        exportDashboardData();
+      } else {
+        alert('Función de exportación no definida.');
+      }
+    });  
